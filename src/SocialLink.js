8af5edbbeca6eb29fcb198c52ed2953dc3b9b236
Frank J Kocina <frank.j.kocina@gmail.com>
2019-01-14 21:37:40 -0600

    fix some styling issues

--- src/SocialLink.js
+++ src/SocialLink.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SocialLink extends Component {
   render() {
     return (
-      <a href={this.props.href} className="col-3" target="_blank" rel="noopener noreferrer">
+      <a href={this.props.href} className="col-3 col-no-pad" target="_blank" rel="noopener noreferrer">
         <img src={this.props.imgSrc} className="social-image" alt={this.props.imgAlt}/>
       </a>
     );
